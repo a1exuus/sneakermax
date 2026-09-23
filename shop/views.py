a1753a6d318226498993beed2a1e688834bbcee2
Alexey <alexexmypride@gmail.com>
2026-09-23 18:23:43 +0300

    added `product_detail`

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
 from staff.models import Worker
+from django.shortcuts import get_object_or_404
 
 
 def start_page(request):
@@ -12,3 +13,26 @@ def process_data(request):
     products = Product.objects.filter(is_popular=True).prefetch_related('image')
     workers = Worker.objects.filter(is_active=True)
     return products, workers
+
+
+def catalog(request):
+    products = Product.objects.all().prefetch_related('image')
+    sizes = []
+    for product in products:
+        size = product.size
+        sizes.append(size)
+    sizes = list(dict.fromkeys(sizes))
+    sizes = sorted(sizes, key=lambda x: int(x.split('-')[0]))
+
+    return render(request, 'catalog.html', {'products': products, 'sizes': sizes})
+
+
+# def cart(request):
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    return render(request, 'product_detail.html', {
+        'product': product,
+    })
--- sneakermax/urls.py
+++ sneakermax/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from shop import views
+from shop import views as shop_views
 import sneakermax.settings as settings
 from django.conf.urls.static import static
 
@@ -8,7 +8,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
-    path('', views.start_page, name='start_page'),
+    path('', shop_views.start_page, name='start_page'),
+    path('catalog/', shop_views.catalog, name='catalog'),
+    path('product/<int:product_id>/', shop_views.product_detail, name='product_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
